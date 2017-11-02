91b02c0650436ab03d26e0face86cae044f08d0f
ArianZargaran <arianzf@gmail.com>
2017-11-01 23:39:08 -0700

    Minutes included

--- js/main.js
+++ js/main.js
@@ -7,21 +7,24 @@ const secondsHand = document.querySelector(".seconds-hand");
 const minutesHand = document.querySelector(".minutes-hand");
 const hoursHand = document.querySelector(".hours-hand");
 
-const digitalClock = document.querySelector(".digital_clock")
-
-function analogClock() {
+function clock() {
+//Get the time
  const now = new Date()
  let seconds = now.getSeconds();
  let minutes = now.getMinutes();
  let hours = now.getHours();
 
+ //Analog Clock
  const secondsDegrees = (seconds / 60) * 360
  const minutesDegrees = (minutes / 60) * 360
- const hoursDegrees = (hours / 12) * 360
+ const hoursDegrees = (hours / 12* 360) + minutes * .5
 
  secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+ //Digital Clock
+ const digitalClock = document.querySelector(".digital_clock")
 
    if (minutes < 10) {minutes = "0" + minutes}
    if (seconds < 10) {seconds = "0" + seconds}
@@ -31,4 +34,4 @@ function analogClock() {
   digitalClock.innerHTML = digitalHour
 }
 
-setInterval(analogClock, 1000)
+setInterval(clock, 1000)
